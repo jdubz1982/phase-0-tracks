b92066d4c095aa3063cd3f51a715bbaf63dc7e85
James B. Wilson <jbwilson1982@gmail.com>
2016-11-16 16:41:07 -0600

    Output stored variables in sentences for user to see.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -29,3 +29,30 @@ if age == ""
 else
   age.to_f
 end
+
+#prints hamsters name
+puts "Your hamster's name is #{name}."
+
+#prints hamsters squeak volume
+puts "Your hamster's squeak volume is #{volume}."
+
+#prints hamsters fur color
+puts "Your hamster has #{fur_color} fur."
+
+#checks to see if hamster is a good candidate for adoption
+if adoption
+  puts "Your hamster is a good candidate for adoption!"
+else 
+  puts "Sadly, your hamster is not a good candidate for adoption."
+end
+
+#checks to see if user specified the hamsters age
+if age == nil
+  puts "You did not specify your hamster's age."
+else
+  puts "Your hamster is #{age} years old."
+end
+
+#thank the user for their input
+puts "Thank you for your information!"
+
