cdcc35f2a0d1945903834dbe2abd65ed2903e7f5
James B. Wilson <jbwilson1982@gmail.com>
2016-11-16 16:11:56 -0600

    Converted user input to proper date type

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,19 +1,31 @@
+#asks user for hamster's name
+puts "What is your hamster's name?"
 name = gets.chomp
 
+#asks user how squeaky your hamster is
 puts "How squeaky is your hamster? (1-10...10 being the loudest)"
-volume = gets.chomp 
+volume = gets.chomp.to_i
 
+#asks user what color fur the hamster has
 puts "What color fur does your hamster have?"
 fur_color = gets.chomp
 
+#asks user if hamster is a good candidate for adoption converts to boolean
 puts "Is your hamster a good candidate for adoption? (y/n)"
 adoption = gets.chomp.upcase
 
+if adoption == "Y"
+  adoption = true
+else
+  adoption = false
+end
+
+#asks user for estimated age, if none given sets age to nil
 puts "What is the estimated age of your hamster?"
 age = gets.chomp
 
 if age == ""
   age = nil
+else
+  age.to_f
 end
-
-p age 
