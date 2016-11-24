bcc9213ea7b7a927937c2deb531550a307e04b18
James B. Wilson <jbwilson1982@gmail.com>
2016-11-23 19:46:32 -0600

    Updated code to convert name if it has numbers in it.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,48 +1,74 @@
-#define method at thats a real_name and converts it to alias
+#define method that takes a real name and converts it to alias
+#alias method will convert real name vowels to the next vowel in an array of vowels
+#alias method then converts real name consonants to next consonant in array of consonants
+#alias method then converts any number to the next number in a numbers array
+#if there are any other characters it will leave them alone ex. ; would be ;
+#alias method will capitalize first and last name
+#alias method will then reverse the first and last name
 
-#def method to convert vowels
+#convert vowels
 def convert_vowels(real_name)
   name_array = real_name.downcase.split(//)
   name_array.map! do |letter|
   vowels = ["a", "e", "i", "o", "u"]
-  #loop through vowels to see if contains letters from real name
-  if vowels.include?(letter)
-  #add 1 to real name index location to get next vowel
-  location = vowels.index(letter) + 1
-    #check for u vowel
-    if vowels[location] == nil
-      letter = "a"
-    else 
-      letter = vowels[location]
+
+    if vowels.include?(letter)
+      location = vowels.index(letter) + 1
+      #check for u 
+      if vowels[location] == nil
+        letter = "a"
+      else 
+        letter = vowels[location]
+      end
+    else
+      letter = letter
     end
-  else
-    letter = letter
-  end
     
-end
+  end
   name_array.join
 end
 
-#def method to convert consonants
+#convert consonants
 def convert_consonant(real_name)
   name_array = real_name.downcase.split(//)
   name_array.map! do |letter|
   consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-  #loop through consonants to see if contains letters from real name
-  if consonants.include?(letter)
-  #add 1 to real name index location to get next consonant 
-  location = consonants.index(letter) + 1
-    #check for z 
-    if consonants[location] == nil
-      letter = "b"
-    else 
-      letter = consonants[location]
+  
+    if consonants.include?(letter)
+    location = consonants.index(letter) + 1
+      #check for z 
+      if consonants[location] == nil
+        letter = "b"
+      else 
+        letter = consonants[location]
+      end
+    else
+      letter = letter
     end
-  else
-    letter = letter
   end
-    
+
+  name_array.join
 end
+
+#convert numbers
+def convert_numbers(real_name)
+  name_array = real_name.downcase.split(//)
+  name_array.map! do |letter|
+  numbers = ["0","1","2","3","4","5","6","7","8","9"]
+    
+    if numbers.include?(letter)
+    location = numbers.index(letter) + 1
+      #check for 9
+      if numbers[location] == nil
+        letter = "0"
+      else
+        letter = numbers[location]
+      end
+    else
+      letter = letter
+    end
+    
+  end
   name_array.join
 end
 
@@ -50,23 +76,23 @@ end
 def capitalize_name(name)
   capital_array = name.split(" ")
   capital_array.map! do |cap_name|
-     cap_name.capitalize!
+     cap_name.capitalize
   end
   capital_array.join(" ")
 end
 
-#def alias_manager to convert vowels and consonants and reverse name
+#call all methods to create alias
 def alias_manager(real_name)
   vowel_name = convert_vowels(real_name)
-  consonant_vowel_name = convert_consonant(vowel_name)
-  new_name = capitalize_name(consonant_vowel_name)
+  consonant_name = convert_consonant(vowel_name)
+  number_name = convert_numbers(consonant_name)
+  new_name = capitalize_name(number_name)
   new_name.split.reverse.join(' ')
 end
 
-#p alias_manager("Felicia Torres")
+#p alias_manager("Felicia Torres") == "Vussit Gimodoe"
+#user interface so multiple names can be entered into a hash
 
-#create user interface so multiple names can be entered and converted
-#set new hash to collect input
 all_alias = {}
 puts "Please enter a name to covert to an alias:"
 real_name = gets.chomp
@@ -82,8 +108,8 @@ all_alias.delete_if { |k, v| k == "q"}
 all_alias.delete_if { |k, v| k == "quit"}
 
 #print each alias and what the real name is
-all_alias.each do |real_name, convert_name|
-  puts "The alias #{convert_name} is actually #{real_name}."
+all_alias.each do |real_name, alias_name|
+  puts "The alias #{alias_name} is actually #{real_name}."
 end
 
 
