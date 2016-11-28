89ddd35bd9468ae528d08b2707d9312597e10f17
James B. Wilson <jbwilson1982@gmail.com>
2016-11-28 14:07:30 -0600

    Add attributes to Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -14,7 +18,32 @@ class Santa
 
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("chocolate chip")
+# santa = Santa.new
+# santa.speak
+# santa.eat_milk_and_cookies("chocolate chip")
+# puts ""
+
+=begin
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+p santas
+=end
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities.sample)
+end
+
+p santas
+
+
+
 
