a9f0065906ae48aae0dd223aec847ad8f488a143
James B. Wilson <jbwilson1982@gmail.com>
2016-12-05 14:24:47 -0600

    Add function to search obj for matching key and value pairs

--- js/algos.js
+++ js/algos.js
@@ -25,5 +25,35 @@ var test = ["house", "car", "television", "computer", "stove"];
 console.log(longest(test));
 
 
+// Release 1: Find a Key-Value Match
+
+// Define function that takes two objects to see if the objects share the same key and value
+  // Define two new arrays using keys from inputted objects
+  // Set a new array to capture matchers
+  // Loop through first array and check to see if its included in the second array
+  // If its included in the second array, add key to matchers array
+  // Otherwise, keep searching
+// Return matchers array
+
+var objOne = {name: "Steven", age: 54, eyes: "brown", hair: "blonde"};
+var objTwo = {name: "Tamir", age: 54, eyes: "blue", hair: "blonde"};
+
+var keyFinder = function(obj1, obj2) {
+  var obj1Keys = Object.keys(obj1);
+  var obj2Keys = Object.keys(obj2);
+  var obj1Values = Object.values(obj1);
+  var obj2Values = Object.values(obj2);
+  var allMatches = {};
+
+  for (var i=0; i < obj1Keys.length; i++)
+    if ((obj2Keys.indexOf(obj1Keys[i]) > -1) && obj2Values.indexOf(obj1Values[i]) > -1) {
+      allMatches[obj1Keys[i]] = obj1Values[i];
+     }
+    
+     return allMatches;
+}
+
+console.log(keyFinder(objOne, objTwo));
+
 
 
